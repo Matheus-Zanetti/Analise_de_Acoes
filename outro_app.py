a79d1c6e7a515a5f979a943770c80314bf81b3f4
Matheus-Zanetti <89101674+Matheus-Zanetti@users.noreply.github.com>
2021-10-22 19:49:03 -0300

    Update outro_app.py

--- outro_app.py
+++ outro_app.py
@@ -1,58 +1,52 @@
 #Importar as bibliotecas
 import datetime
+import sqlite3
+
 import yfinance as yf
 import pandas as pd
 from datetime import date
 import streamlit as st
 import datetime as dt
 import numpy as np
+import plotly.graph_objs as go
+
 
 #Ler e tratar dados
-dados = r'C:\Users\mathe\Desktop\Projeto23\App\Tabela_ações.csv'
-df = pd.read_csv(dados)
-df = df.dropna()
+conn = sqlite3.connect('banco_acoes.db')
+df = pd.read_sql('SELECT * FROM acoes', con=conn)
+conn.close()
 
+df.drop(columns=['index'], inplace=True)
+df['Data'] = pd.to_datetime(df['Data'])
 
 
 st.header('WebApp Ações')
 
 st.subheader('Visualizar ações')
 
-acoes = df.columns[:(len(df.columns) - 1)]
+acoes = df.columns[1:]
 
 st.sidebar.header('Parâmetros')
 choice_acoes = st.sidebar.multiselect('Selecione a sua ação', acoes)
 
 data = pd.to_datetime(df['Data'])
-#min_data = pd.to_datetime(min(data))
-#max_data = pd.to_datetime(max(data))
+min_data = pd.to_datetime(min(data))
+max_data = pd.to_datetime(max(data))
 
-data_start = st.sidebar.date_input("Data inicial", value = pd.to_datetime('01-01-2010'))
-data_end = st.sidebar.date_input("Data final", value = pd.to_datetime('today'))
+data_start = st.sidebar.date_input('Data inicial', min_data)
+data_end = st.sidebar.date_input('Data final', max_data)
 
-start = pd.to_datetime(data_start).date()
-end = pd.to_datetime(data_end).date()
+start = pd.to_datetime(data_start)
+end = pd.to_datetime(data_end)
 
+intervalo_data = df[(df['Data'] >= start) & (df['Data'] <= end)]
+intervalo_data = intervalo_data.set_index('Data')
+
+dados = intervalo_data[choice_acoes]
 
 if data_end < data_start:
     st.error('Data inicial maior que data final')
 
 if len(choice_acoes) > 0:
-    stock = yf.download(choice_acoes, start=start, end=end)
-    st.line_chart(stock['Adj Close'])
+    st.line_chart(dados)
 
-    fechamento = stock['Adj Close']
-
-
-    #stock_normalizado = stock.copy()
-    #for i in stock_normalizado:
-    #    stock_normalizado = np.log(stock['Adj Close'][len(stock)-1] / stock['Adj Close'][0])
-    #    stock_normalizado = pd
-    #    st.dataframe(stock_normalizado)
-
-
-
-
-
-
-#Visualização de dados
